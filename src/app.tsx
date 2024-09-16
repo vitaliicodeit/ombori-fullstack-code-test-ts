1114576050191b8026e9953e73d8ba15303c0c92
vitalii.diachenko <v.di4chenko@gmail.com>
2024-09-16 17:32:33 +0100

    [build][refactor] setup ESLint with Prettier and clean up app.tsx and index.tsx files

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import styles from './app.module.scss';
+
+const App: FC = () => {
+  return <div className={styles.App}>It&apos;s working!</div>;
+};
+
+export { App };
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { App } from './app';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootNode = document.getElementById('root');
+
+if (rootNode) {
+  const root = createRoot(rootNode);
+
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+} else {
+  throw new Error('Root container does not exist');
+}
